b768497c5f825084cfb7a2f1a70a7997e0069e29
Ido <gershido@gmail.com>
2023-08-29 20:03:59 +0300

    adjust modules, add deploy scripts

--- src/DecentralistEligibilityModule.sol
+++ src/DecentralistEligibilityModule.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.18;
+pragma solidity ^0.8.19;
 
 // import { console2 } from "forge-std/Test.sol"; // remove before deploy
-import {HatsEligibilityModule, HatsModule} from "hats-module/HatsEligibilityModule.sol";
-import {DecentralistInterface} from "decentralist/DecentralistInterface.sol";
+import { HatsEligibilityModule, HatsModule } from "hats-module/HatsEligibilityModule.sol";
+import { DecentralistInterface } from "decentralist/DecentralistInterface.sol";
 
 /**
  * @title DecentralistEligibility
@@ -12,50 +12,52 @@ import {DecentralistInterface} from "decentralist/DecentralistInterface.sol";
  */
 
 contract DecentralistEligibility is HatsEligibilityModule {
-    /*//////////////////////////////////////////////////////////////
+  /*//////////////////////////////////////////////////////////////
                           PUBLIC CONSTANTS
     //////////////////////////////////////////////////////////////*/
 
-    /**
-     * See: https://github.com/Hats-Protocol/hats-module/blob/main/src/HatsModule.sol
-     * --------------------------------------------------------------------+
-     * CLONE IMMUTABLE "STORAGE"                                           |
-     * --------------------------------------------------------------------|
-     * Offset  | Constant        | Type    | Length  |                     |
-     * --------------------------------------------------------------------|
-     * 0       | IMPLEMENTATION  | address | 20      |                     |
-     * 20      | HATS            | address | 20      |                     |
-     * 40      | hatId           | uint256 | 32      |                     |
-     * 72      | LIST_ADDRESS    | address | 20      |                     |
-     * --------------------------------------------------------------------+
-     */
+  /**
+   * See: https://github.com/Hats-Protocol/hats-module/blob/main/src/HatsModule.sol
+   * --------------------------------------------------------------------+
+   * CLONE IMMUTABLE "STORAGE"                                           |
+   * --------------------------------------------------------------------|
+   * Offset  | Constant        | Type    | Length  |                     |
+   * --------------------------------------------------------------------|
+   * 0       | IMPLEMENTATION  | address | 20      |                     |
+   * 20      | HATS            | address | 20      |                     |
+   * 40      | hatId           | uint256 | 32      |                     |
+   * 72      | LIST_ADDRESS    | address | 20      |                     |
+   * --------------------------------------------------------------------+
+   */
 
-    /// The address of the Decentralist contract used to check eligibility
-    function LIST_ADDRESS() public pure returns (address) {
-        return _getArgAddress(72);
-    }
+  /// The address of the Decentralist contract used to check eligibility
+  function LIST_ADDRESS() public pure returns (address) {
+    return _getArgAddress(72);
+  }
 
-    /*//////////////////////////////////////////////////////////////
+  /*//////////////////////////////////////////////////////////////
                                 INITIALIZER
     //////////////////////////////////////////////////////////////*/
-    function setUp(bytes calldata _initData) public override initializer {
-        //not used currently TODO: delete?
-    }
+  function _setUp(bytes calldata _initData) internal override {
+    //not used currently TODO: delete?
+  }
 
-    /*//////////////////////////////////////////////////////////////
+  /*//////////////////////////////////////////////////////////////
                             CONSTRUCTOR
     //////////////////////////////////////////////////////////////*/
-    constructor(string memory _version) HatsModule(_version) {}
+  constructor(string memory _version) HatsModule(_version) { }
 
-    /*//////////////////////////////////////////////////////////////
+  /*//////////////////////////////////////////////////////////////
                         HATS ELIGIBILITY FUNCTION
     //////////////////////////////////////////////////////////////*/
-    ///
-    function getWearerStatus(
-        address _wearer,
-        uint256 /*_hatId */
-    ) public view override returns (bool eligible, bool standing) {
-        eligible = DecentralistInterface(LIST_ADDRESS()).onList(_wearer);
-        standing = true;
-    }
+  ///
+  function getWearerStatus(address _wearer, uint256 /*_hatId */ )
+    public
+    view
+    override
+    returns (bool eligible, bool standing)
+  {
+    eligible = DecentralistInterface(LIST_ADDRESS()).onList(_wearer);
+    standing = true;
+  }
 }
